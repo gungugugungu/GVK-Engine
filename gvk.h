966cce48dcf27c20c21da3dc56363e74f9d2bc34
Gungu <ddoggames7@gmail.com>
2026-04-06 11:14:41 +0200

    linked vulkan

--- gvk.h
+++ gvk.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include <vulkan/vulkan.h>
 
 namespace gvk {
     void init();
@@ -11,11 +12,17 @@ namespace gvk {
 
 namespace gvk {
     inline SDL_Window* window = nullptr;
+    inline VkInstance instance = VK_NULL_HANDLE;
+    inline VkApplicationInfo application_info = {};
+
     void init() {
+        // --- SDL SETUP ---
         SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD | SDL_INIT_AUDIO);
         SDL_Rect display_bounds;
         SDL_GetDisplayBounds(SDL_GetPrimaryDisplay(), &display_bounds);
         window = SDL_CreateWindow("GVK RENDERER", display_bounds.x, display_bounds.y, SDL_WINDOW_VULKAN | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_FULLSCREEN);
+
+        // --- VULKAN SETUP ---
     }
 
     void quit() {
