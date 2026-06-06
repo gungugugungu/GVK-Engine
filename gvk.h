0068503c2b347095205e31e5a5136e7ca435d007
Gungu <ddoggames7@gmail.com>
2026-06-06 15:35:41 +0200

    started working on descriptor setup, I hate this so much more than sokol

--- gvk.h
+++ gvk.h
@@ -29,6 +29,8 @@ abort(); \
 } \
 } while (0)
 
+using namespace std;
+
 VkCommandPoolCreateInfo init_command_pool_create_info(uint32_t queueFamilyIndex,
     VkCommandPoolCreateFlags flags)
 {
@@ -253,6 +255,107 @@ struct AllocatedImage {
     VkFormat format;
 };
 
+struct DescriptorLayoutBuilder {
+    std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+    void add_binding(uint32_t binding, VkDescriptorType type);
+    void clear();
+    VkDescriptorSetLayout build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext = nullptr, VkDescriptorSetLayoutCreateFlags flags = 0);
+};
+
+void DescriptorLayoutBuilder::add_binding(uint32_t binding, VkDescriptorType type)
+{
+    VkDescriptorSetLayoutBinding newbind {};
+    newbind.binding = binding;
+    newbind.descriptorCount = 1;
+    newbind.descriptorType = type;
+
+    bindings.push_back(newbind);
+}
+
+void DescriptorLayoutBuilder::clear()
+{
+    bindings.clear();
+}
+
+VkDescriptorSetLayout DescriptorLayoutBuilder::build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext, VkDescriptorSetLayoutCreateFlags flags)
+{
+    for (auto& b : bindings) {
+        b.stageFlags |= shaderStages;
+    }
+
+    VkDescriptorSetLayoutCreateInfo info = {.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO};
+    info.pNext = pNext;
+
+    info.pBindings = bindings.data();
+    info.bindingCount = (uint32_t)bindings.size();
+    info.flags = flags;
+
+    VkDescriptorSetLayout set;
+    VK_CHECK(vkCreateDescriptorSetLayout(device, &info, nullptr, &set));
+
+    return set;
+}
+
+struct DescriptorAllocator {
+
+    struct PoolSizeRatio{
+        VkDescriptorType type;
+        float ratio;
+    };
+
+    VkDescriptorPool pool;
+
+    void init_pool(VkDevice device, uint32_t maxSets, std::span<PoolSizeRatio> poolRatios);
+    void clear_descriptors(VkDevice device);
+    void destroy_pool(VkDevice device);
+
+    VkDescriptorSet allocate(VkDevice device, VkDescriptorSetLayout layout);
+};
+
+void DescriptorAllocator::init_pool(VkDevice device, uint32_t maxSets, std::span<PoolSizeRatio> poolRatios)
+{
+    std::vector<VkDescriptorPoolSize> poolSizes;
+    for (PoolSizeRatio ratio : poolRatios) {
+        poolSizes.push_back(VkDescriptorPoolSize{
+            .type = ratio.type,
+            .descriptorCount = uint32_t(ratio.ratio * maxSets)
+        });
+    }
+
+    VkDescriptorPoolCreateInfo pool_info = {.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO};
+    pool_info.flags = 0;
+    pool_info.maxSets = maxSets;
+    pool_info.poolSizeCount = (uint32_t)poolSizes.size();
+    pool_info.pPoolSizes = poolSizes.data();
+
+    vkCreateDescriptorPool(device, &pool_info, nullptr, &pool);
+}
+
+void DescriptorAllocator::clear_descriptors(VkDevice device)
+{
+    vkResetDescriptorPool(device, pool, 0);
+}
+
+void DescriptorAllocator::destroy_pool(VkDevice device)
+{
+    vkDestroyDescriptorPool(device,pool,nullptr);
+}
+
+VkDescriptorSet DescriptorAllocator::allocate(VkDevice device, VkDescriptorSetLayout layout)
+{
+    VkDescriptorSetAllocateInfo allocInfo = {.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO};
+    allocInfo.pNext = nullptr;
+    allocInfo.descriptorPool = pool;
+    allocInfo.descriptorSetCount = 1;
+    allocInfo.pSetLayouts = &layout;
+
+    VkDescriptorSet ds;
+    VK_CHECK(vkAllocateDescriptorSets(device, &allocInfo, &ds));
+
+    return ds;
+}
+
 namespace gvk {
     void init();
     void quit();
@@ -299,6 +402,11 @@ namespace gvk {
     // draw resources
     inline AllocatedImage _draw_image;
     inline VkExtent2D _draw_extent;
+
+    // descriptors
+    DescriptorAllocator global_descriptor_allocator;
+    VkDescriptorSet _draw_image_descriptors;
+    VkDescriptorSetLayout _draw_image_descriptor_layout;
 
     void draw_background (VkCommandBuffer cmd) {
         VkClearColorValue clear_color_value;
@@ -444,6 +552,45 @@ namespace gvk {
             VK_CHECK(vkCreateSemaphore(_vk_device, &semaphore_create_info, nullptr, &_frames[i]._swapchain_semaphore));
             VK_CHECK(vkCreateSemaphore(_vk_device, &semaphore_create_info, nullptr, &_frames[i]._render_semaphore));
         }
+    }
+
+    void init_descriptors() {
+        // descriptor pool of 10 sets with 1 image each
+        vector<DescriptorAllocator::PoolSizeRatio> sizes = {
+            {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1}
+        };
+
+        global_descriptor_allocator.init_pool(_vk_device, 10, sizes);
+
+        // descriptor set layout for compute draw
+        {
+            DescriptorLayoutBuilder builder;
+            builder.add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE);
+            _draw_image_descriptor_layout = builder.build(_vk_device, VK_SHADER_STAGE_COMPUTE_BIT);
+        }
+
+        _draw_image_descriptors = global_descriptor_allocator.allocate(_vk_device, _draw_image_descriptor_layout);
+        VkDescriptorImageInfo img_info{};
+        img_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        img_info.imageView = _draw_image.image_view;
+
+        VkWriteDescriptorSet draw_image_write = {};
+        draw_image_write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        draw_image_write.pNext = nullptr;
+
+        draw_image_write.dstBinding = 0;
+        draw_image_write.dstSet = _draw_image_descriptors;
+        draw_image_write.descriptorCount = 1;
+        draw_image_write.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+        draw_image_write.pImageInfo = &img_info;
+
+        vkUpdateDescriptorSets(_vk_device, 1, &draw_image_write, 0, nullptr);
+
+        _main_deletion_queue.push_function([&]() {
+           global_descriptor_allocator.destroy_pool(_vk_device);
+
+            vkDestroyDescriptorSetLayout(_vk_device, _draw_image_descriptor_layout, nullptr);
+        });
     }
 
     void init() {
